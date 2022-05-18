f42ff95486d4734ad6138b52f2b1b15bd0b47cae
paul-goodall <paul.thomas.goodall@gmail.com>
2022-05-18 22:51:11 +1000

    adding the init files x11

--- magic_installs/magic_init_mac.sh
+++ magic_installs/magic_init_mac.sh
@@ -3,27 +3,27 @@
 echo "================ Entering magic_init_mac.sh ================"
 # Persistent home folder hack:
 com=( rm -rf /home/rstudio ; ln -s ${MAGIC_FOLDER}/$DEFAULT_USER /home/$DEFAULT_USER )
-echo "${cmd[@]}"
-"${cmd[@]}"
+echo "$cmd"
+eval "$cmd"
 
 com=( mkdir -p /home/$DEFAULT_USER/.jupyter )
-echo "${cmd[@]}"
-"${cmd[@]}"
+echo "$cmd"
+eval "$cmd"
 
 com=( mv /jupyter_notebook_config.json /home/$DEFAULT_USER/.jupyter/jupyter_notebook_config.json )
-echo "${cmd[@]}"
-"${cmd[@]}"
+echo "$cmd"
+eval "$cmd"
 
 com=( chmod -R 755 /home/$DEFAULT_USER/.jupyter )
-echo "${cmd[@]}"
-"${cmd[@]}"
+echo "$cmd"
+eval "$cmd"
 
 com=( cd ${MAGIC_FOLDER} )
-echo "${cmd[@]}"
-"${cmd[@]}"
+echo "$cmd"
+eval "$cmd"
 
 com=( su $DEFAULT_USER -c "/opt/venv/reticulate/bin/jupyter notebook --no-browser --ip=0.0.0.0 --port=8888 --allow-root &" )
-echo "${cmd[@]}"
-"${cmd[@]}"
+echo "$cmd"
+eval "$cmd"
 
 echo "================ Finished magic_init_mac.sh ================"
