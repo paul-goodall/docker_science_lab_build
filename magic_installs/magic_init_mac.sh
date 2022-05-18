b9519f0b1b12055cb826bb502e881bd4b5434bef
paul-goodall <paul.thomas.goodall@gmail.com>
2022-05-18 23:36:14 +1000

    adding the init files x14

--- magic_installs/magic_init_mac.sh
+++ magic_installs/magic_init_mac.sh
@@ -6,7 +6,7 @@ com="rm -rf /home/rstudio ; ln -s ${MAGIC_FOLDER}/$DEFAULT_USER /home/$DEFAULT_U
 echo "$com"
 eval "$com"
 
-com="mkdir -p /home/$DEFAULT_USER/.jupyter"
+com="cd /home/$DEFAULT_USER ; mkdir .jupyter"
 echo "$com"
 eval "$com"
 
@@ -18,11 +18,11 @@ com="chmod -R 755 /home/$DEFAULT_USER/.jupyter"
 echo "$com"
 eval "$com"
 
-com=( cd ${MAGIC_FOLDER} )
+com="cd ${MAGIC_FOLDER}"
 echo "$com"
 eval "$com"
 
-com=( su $DEFAULT_USER -c "/opt/venv/reticulate/bin/jupyter notebook --no-browser --ip=0.0.0.0 --port=8888 --allow-root &" )
+com="su $DEFAULT_USER -c '/opt/venv/reticulate/bin/jupyter notebook --no-browser --ip=0.0.0.0 --port=8888 --allow-root &'"
 echo "$com"
 eval "$com"
 
