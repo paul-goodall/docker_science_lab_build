38da1504fb7cf9e005798f8fd7aaddda6bc76580
paul-goodall <paul.thomas.goodall@gmail.com>
2022-05-21 14:40:02 +1000

    updated the buildfile to build and push latest tag also

--- goodsy_dockerbuild_icing.sh
+++ goodsy_dockerbuild_icing.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
 
 docker_file="incremental_dockerfiles/Icing_Dockerfile"
-image_name="goodsy/data_science_lab:u20_r420_202205"
+image_base="docker_data_science_lab"
+image_name="goodsy/${image_base}:u20_r420_$(date +%Y%m)
 docker_stamp="Dockerfile_$(date +%Y.%m.%d-%H.%M.%S)"
 
 docker build -f $docker_file \
              --build-arg docker_stamp=$docker_stamp \
              --build-arg docker_file=$docker_file \
              -t $image_name .
+
+docker push $image_name
+
+docker tag $image_name goodsy/${image_base}:latest
+docker push goodsy/${image_base}:latest
+
 
 # =============
 # add the following into your dockerfile just after the FROM line:
